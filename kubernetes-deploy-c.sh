dfae22296462f8bca6a62df4aaa745c3c13c5aa0
John Miller <john.miller@solo.io>
2023-10-23 21:09:45 -0400

    Added K8s deplotment for a 3rd cluster

---
+++ kubernetes-deploy-c.sh
@@ -0,0 +1,25 @@
+
+##!/bin/bash
+
+#
+# Set Kubernetes context to cluster B using 'kubectl config use-context' before running these commands
+#
+
+#
+# Deploy the AWS load balancer controller
+#
+CLUSTER_NAME="EKS-CLUSTER-C"
+helm install aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system \
+--set clusterName=$CLUSTER_NAME \
+--set serviceAccount.create=false \
+--set serviceAccount.name=aws-load-balancer-controller
+
+#
+# Deploy services
+# This will provision an internal NLB
+#
+## /!\ WARNING /!\
+## values below will only work with the "AWS Load Balancer Controller". Not with the default k8s in-tree controller
+# service.beta.kubernetes.io/aws-load-balancer-type: "external" # https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.2/guide/service/nlb/#configuration
+# service.beta.kubernetes.io/aws-load-balancer-scheme: internal # https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.2/guide/service/nlb/
+# service.beta.kubernetes.io/aws-load-balancer-nlb-target-type: "instance" # https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.2/guide/service/nlb/#instance-mode_1
